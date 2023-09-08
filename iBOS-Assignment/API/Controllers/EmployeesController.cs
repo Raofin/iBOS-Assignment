9984351c05e7778331c220be359e330482630469
Raofin <zaidaminraofin@gmail.com>
2023-09-08 23:34:24 +0600

    Remove extra controllers

--- iBOS-Assignment/API/Controllers/EmployeesController.cs
+++ iBOS-Assignment/API/Controllers/EmployeesController.cs
@@ -22,8 +22,8 @@ namespace iBOS_Assignment.API.Controllers
         {
             _employeeService = employeeService;
         }
-        
-        // PUT: api/Employees/UpdateNameAndCode/5
+
+        // PUT: api/Employees/UpdateNameAndCode/{employeeId}
         [HttpPut("UpdateNameAndCode/{id}")]
         public IActionResult UpdateNameAndCode(long id, [FromBody] EmpNameAndCodeDto employee)
         {
@@ -34,7 +34,7 @@ namespace iBOS_Assignment.API.Controllers
                 return NotFound();
             }
 
-            // Update only the EmployeeName and EmployeeCode properties
+            // Update the EmployeeName and EmployeeCode properties
             if (!string.IsNullOrEmpty(employee.EmployeeName))
             {
                 existingEmployee.EmployeeName = employee.EmployeeName;
@@ -90,107 +90,6 @@ namespace iBOS_Assignment.API.Controllers
             }
 
             return Ok(hierarchy);
-        }
-
-        // GET: api/Employees
-        [HttpGet]
-        public IActionResult Get()
-        {
-            var employee = _employeeService.Get();
-
-            return Ok(employee);
-        }
-
-        // GET: api/Employees/5
-        [HttpGet("{id}")]
-        public IActionResult Get(long id)
-        {
-            var employee = _employeeService.Get(id);
-
-            if (employee == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(employee);
-        }
-
-        // PUT: api/Employees/5
-        [HttpPut("{id}")]
-        public IActionResult Put(long id, [FromBody] EmployeeDto employee)
-        {
-            EmployeeDto existingEmployee = _employeeService.Get(id);
-
-            if (existingEmployee == null)
-            {
-                return NotFound();
-            }
-
-            // Update only the properties that are provided in the request
-            if (employee.EmployeeName != null)
-            {
-                existingEmployee.EmployeeName = employee.EmployeeName;
-            }
-
-            if (employee.EmployeeCode != null)
-            {
-                existingEmployee.EmployeeCode = employee.EmployeeCode;
-            }
-
-            if (employee.EmployeeSalary != default(int))
-            {
-                existingEmployee.EmployeeSalary = employee.EmployeeSalary;
-            }
-
-            if (employee.SupervisorId.HasValue)
-            {
-                existingEmployee.SupervisorId = employee.SupervisorId;
-            }
-
-            var updatedEmployee = _employeeService.Update(existingEmployee);
-
-            return Ok(updatedEmployee);
-        }
-
-        // DELETE: api/Employees/5
-        [HttpDelete("{id}")]
-        public IActionResult Delete(long id)
-        {
-            var existingEmployee = _employeeService.Get(id);
-
-            return existingEmployee == null 
-                ? (IActionResult)NotFound() 
-                : Ok(_employeeService.Delete(id));
-        }
-
-        // POST: api/Employees
-        [HttpPost]
-        public IActionResult Post([FromBody] EmployeeDto employee)
-        {
-            if (employee == null)
-            {
-                return BadRequest("Invalid employee data");
-            }
-
-            var creationSuccess = _employeeService.Create(employee);
-
-            if (creationSuccess)
-            {
-                return Ok("Employee created successfully");
-            }
-            else
-            {
-                return BadRequest("Employee creation failed");
-            }
-        }
-
-        // GET: api/Employees/Exists/5
-        [HttpGet("Exists/{id}")]
-        public IActionResult Exists(long id)
-        {
-            var exists = _employeeService.Exists(id);
-
-            return Ok(exists);
         }
     }
 }
